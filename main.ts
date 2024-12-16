d169622039034cdc9879f357afc262a1cbfecce8
Brett <bjabramczyk@gmail.com>
2024-12-16 10:43:13 -0600

    properly throw on error

--- main.ts
+++ main.ts
@@ -13,51 +13,57 @@ import { verifyPage } from "./src/sanity-checks.ts";
 import { prioritizePlayers } from "./src/prioritization.ts";
 
 (async () => {
-  if (!verifyPage()) {
-    return;
-  }
-
-  console.clear();
-
-  const players = await getPlayers();
-  console.table(players);
-  setAllPlayersToBench(players);
-
-  let numPlayersStarted = 0;
-
-  prioritizePlayers(players).forEach(({ player, score }) => {
-    insertPlayerPredictedScore(player, score, {
-      isDtd: player.playerStatus === "DTD",
-      noGame: !player.opponentInfo,
-    });
-    // if (player.refinedPlayerStatus) {
-    //   insertRefinedPlayerStatus(player, player.refinedPlayerStatus);
-    // }
-
-    if (numPlayersStarted >= 9) {
+  try {
+    if (!verifyPage()) {
       return;
     }
 
-    if (player.isTaxi || player.isIr) {
-      stylePlayerAsUnableToStart(player);
-      return;
-    }
+    console.clear();
 
-    const isInjured =
-      player.playerStatus === "DTD" || player.playerStatus === "OUT";
-    if (isInjured) {
-      stylePlayerAsPossiblyInjured(player);
-    }
+    const players = await getPlayers();
+    console.table(players);
+    setAllPlayersToBench(players);
 
-    const playerStarted = startPlayer(player, {
-      isAlternate: numPlayersStarted >= 9,
+    let numPlayersStarted = 0;
+
+    prioritizePlayers(players).forEach(({ player, score }) => {
+      insertPlayerPredictedScore(player, score, {
+        isDtd: player.playerStatus === "DTD",
+        noGame: !player.opponentInfo,
+      });
+      // if (player.refinedPlayerStatus) {
+      //   insertRefinedPlayerStatus(player, player.refinedPlayerStatus);
+      // }
+
+      if (numPlayersStarted >= 9) {
+        return;
+      }
+
+      if (player.isTaxi || player.isIr) {
+        stylePlayerAsUnableToStart(player);
+        return;
+      }
+
+      const isInjured =
+        player.playerStatus === "DTD" || player.playerStatus === "OUT";
+      if (isInjured) {
+        stylePlayerAsPossiblyInjured(player);
+      }
+
+      const playerStarted = startPlayer(player, {
+        isAlternate: numPlayersStarted >= 9,
+      });
+      if (playerStarted) {
+        numPlayersStarted++;
+      }
     });
-    if (playerStarted) {
-      numPlayersStarted++;
-    }
-  });
 
-  if (lineupHasChanges()) {
-    // saveLineup();
+    if (lineupHasChanges()) {
+      // saveLineup();
+    }
+  } catch (error) {
+    console.error(error);
+    alert("Script failed:" + error);
+    throw error;
   }
 })();
